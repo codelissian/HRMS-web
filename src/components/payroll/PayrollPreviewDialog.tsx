95ae9a6292da1718670122d55e3b858bca644355
Ankush <ankushoffice5@gmail.com>
2025-11-21 15:57:48 +0530

    refactor: Reduce dialog size and use zoom property for preview
    - Reduce dialog size to max-w-4xl and max-h-[85vh]
    - Replace transform scale with CSS zoom property (0.6)
    - Simplify styling by removing transform origin and width adjustments
    - Keep dialog at reasonable size while content is zoomed out

--- src/components/payroll/PayrollPreviewDialog.tsx
+++ src/components/payroll/PayrollPreviewDialog.tsx
@@ -21,12 +21,15 @@ export function PayrollPreviewDialog({
 }: PayrollPreviewDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-[95vw] max-h-[95vh] overflow-hidden flex flex-col p-0">
+      <DialogContent className="max-w-4xl max-h-[85vh] overflow-hidden flex flex-col p-0">
         <DialogHeader className="px-6 pt-6 pb-4">
           <DialogTitle>Payroll Preview</DialogTitle>
         </DialogHeader>
         <div className="flex-1 overflow-auto border-t bg-gray-50 p-4">
-          <div className="bg-white shadow-sm rounded-lg overflow-hidden" style={{ transform: 'scale(0.75)', transformOrigin: 'top left', width: '133.33%' }}>
+          <div 
+            className="bg-white shadow-sm rounded-lg overflow-hidden"
+            style={{ zoom: 0.6 }}
+          >
             <div
               dangerouslySetInnerHTML={{ __html: htmlContent }}
               className="payroll-preview"
